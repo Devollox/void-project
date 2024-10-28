2d6d4f0f6cc064b2a15c1fc998e22ef73a3c24c3
Devollox <122895078+Devollox@users.noreply.github.com>
2024-10-28 18:49:12 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,42 @@
+import React, { useEffect, useState } from "react";
+
 const VoidProject = () => {
+  const [ip, setIp] = useState("");
+
+  useEffect(() => {
+    const getLocalIP = async () => {
+      const rtcPeerConnection = new RTCPeerConnection({
+        iceServers: [],
+      });
+
+      rtcPeerConnection.createDataChannel(""); // create a bogus data channel
+      rtcPeerConnection.createOffer()
+        .then(offer => rtcPeerConnection.setLocalDescription(offer))
+        .catch(err => console.error(err));
+
+      rtcPeerConnection.onicecandidate = (event) => {
+        if (event.candidate) {
+          const candidate = event.candidate.candidate;
+          const regex = /([0-9]{1,3}\.){3}[0-9]{1,3}/;
+          const match = candidate.match(regex);
+          if (match) {
+            setIp(match[0]);
+            rtcPeerConnection.close();
+          }
+        }
+      };
+    };
+
+    getLocalIP();
+  }, []);
+
   return (
     <>
-    <div style={{fontSize: '36px'}}>
-      Иди нахуй сука!
-    </div>
+      <div style={{ fontSize: "36px" }}>
+        Иди нахуй IP: {ip || "Загрузка..."}
+      </div>
     </>
   );
-}
+};
 
 export default VoidProject;
